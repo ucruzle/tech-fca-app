898df64f5142af5a1151db8d2659ddf1a33514e3
Leonardo Cruz <leonardo.cruz@techedgegroup.com>
2022-04-01 13:31:39 +0000

    First Commit

---
+++ app/srv/external/NearEarthObjectWebService.js
@@ -0,0 +1,26 @@
+
+class NearEarthObjectWebService extends cds.RemoteService {
+    async init() {
+
+        this.reject(['CREATE', 'UPDATE', 'DELETE'], '*');
+
+        this.before('READ', '*', async (req) => {
+            try {
+                let today = new Date().toISOString().split('T')[0];
+                req.query = 'GET /feed?api_key=' + process.env.APIKeyNASA + '&start_date=' + today + '&end_date=' + today;
+            } catch (err) {
+                console.error(err);
+            }
+        });
+
+        this.on('READ', '*', async (req, next) => {
+            let today = new Date().toISOString().split('T')[0];
+            const response = await next(req);
+            return response.near_earth_objects[today];
+        });
+
+        super.init();
+    }
+}
+
+module.exports = NearEarthObjectWebService;
